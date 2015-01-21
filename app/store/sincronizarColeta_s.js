d92832113298eaf28a205f3557ea92783f5dbb69
FernandoMatias <fernando.matias@sisdado.com.br>
2015-01-21 15:40:24 -0200

    Update sincronizarColeta_s.js

--- app/store/sincronizarColeta_s.js
+++ app/store/sincronizarColeta_s.js
@@ -1,10 +1,10 @@
 var store2 = Ext.create('Touch2Demo.store.IpSincronizar_s');
 	 arquivo = store2.data.get(1);
 	 if (arquivo == null){
-	 link = 'app/php/listaContatos.php';    
+	 link = 'app/php/criarColeta.php';    
 	 }else{
 	    ip =arquivo.data.ip;	
-	    link ='http://'+ip+'/php/listaContatos.php';
+	    link ='http://'+ip+'/php/criarColeta.php';
 	}
 
 Ext.define('Touch2Demo.store.sincronizarColeta_s', {
@@ -15,9 +15,10 @@ Ext.define('Touch2Demo.store.sincronizarColeta_s', {
 	    type: 'ajax',
 	    //url: 'php/json/listaContatos.php',
 	    api: {
-		create: 'app/php/criarColeta.php', //CRUD
-		//read : 'app/php/listaContatos.php',
-		read: link,
+	//	create: 'app/php/criarColeta.php', //CRUD
+		create: link,
+		read : 'app/php/listaContatos.php',
+		//read: link,
 		update: 'app/php/json/atualizaContato.php',
 		destroy: 'php/json/deletaContato.php'
 	    },
